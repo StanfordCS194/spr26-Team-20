7c3a36f4f58bcaf36df85999081c173a0b4c382d
Felipe <mbgroff@stanford.edu>
2026-06-09 17:02:31 -0700

    Read Firebase credentials from env var

--- server/server.ts
+++ server/server.ts
@@ -16,17 +16,18 @@ import type { MessageDocument, PrinterDocument } from "./database_names.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const envDir = join(__dirname, "env");
-const envFiles = readdirSync(envDir).filter(file => file.endsWith('.json'));
+// const envDir = join(__dirname, "env");
+// const envFiles = readdirSync(envDir).filter(file => file.endsWith('.json'));
 
-const envFile = envFiles[0];
-if (!envFile) {
-  throw new Error("No JSON files found in env directory");
-}
+// const envFile = envFiles[0];
+// if (!envFile) {
+//   throw new Error("No JSON files found in env directory");
+// }
 
-const serviceAccountPath = join(envDir, envFile);
+// const serviceAccountPath = join(envDir, envFile);
 
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf-8"));
+// const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf-8"));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
 
 const adminApp = initializeApp({
   credential: cert(serviceAccount),
